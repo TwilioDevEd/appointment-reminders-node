7bb30bbd326ca286db51b29b141722c177884e53
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-11 12:39:06 -0600

    Check for connection string before trying to connect Should throw appropriate error

--- test/connectionHelper.js
+++ test/connectionHelper.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const cfg = require("../config");
 
+if (!cfg.mongoUrlTest) {
+  throw new Error("MONGO_URL_TEST env variable not set.");
+}
+
 exports.mongoConnection = mongoose.connect(cfg.mongoUrlTest);
 mongoose.Promise = Promise;
