bd7ac1a89b1ffcddc19ff1159eb6e2c0b3acee8c
Alexis Crespo <acrespo@twilio.com>
2021-10-28 13:06:23 -0500

    Change error handler to not send stack trace to the user

--- app.js
+++ app.js
@@ -35,23 +35,10 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-    });
-  });
-}
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  console.error(err);
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
